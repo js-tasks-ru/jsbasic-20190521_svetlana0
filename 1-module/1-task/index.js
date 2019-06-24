59b51f52d696ef1e4124858a4336cfb3e31ca18c
Sweetpl <svetlanavitg@gmail.com>
2019-06-24 22:06:46 +0300

    task-1, pow(n,m)

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,6 +4,17 @@
  * @param {number} n index
  * @returns {number}
  */
+ "use strict";
+ var str = +prompt("x", '2');
+ var t= +prompt("n", '3');
 function pow (m, n) {
 
+    function pow(x,n){
+        if (n != 1) {
+            return x * pow(x, n - 1);
+        } else {
+            return x;
+        }
+    };
+    console.log (pow (str,t));
 }
