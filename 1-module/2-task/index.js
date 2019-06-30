90b3b76ca9ef5965fd9153a5e4fb9482b7ce0567
Sweetpl <svetlanavitg@gmail.com>
2019-06-30 22:48:53 +0300

    исправлено

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -11,9 +11,19 @@ function print (text) {
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
  */
-function isValid (name) {
-}
 
+function isValid (name) {
+    if (name === null || name === undefined)  {
+        return false
+    }
+    else if (typeof name === "string") {
+        if (name.length<4 ||  name.indexOf(" ")!==-1 )  {
+            return false
+        } else return true
+    } else {
+        return true
+    }
+}
 function sayHello () {
     let userName = prompt('Введите ваше имя');
 
